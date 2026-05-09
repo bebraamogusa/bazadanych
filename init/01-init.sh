4de2dc4bc333a35fe7cf11565e1963f62a1cc55a
bebraamogusa <140707898+bebraamogusa@users.noreply.github.com>
2026-05-09 22:54:00 +0200

    Update 01-init.sh

--- init/01-init.sh
+++ init/01-init.sh
@@ -7,12 +7,12 @@ mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "SET GLOBAL local_infile = 1;"
 echo "==================================="
 echo "1. Creating schema and tables..."
 echo "==================================="
-mysql -u root -p"${MYSQL_ROOT_PASSWORD}" < "/import_danych/nawigacja_gwiezdna.sql"
+mysql --default-character-set=utf8mb4 -u root -p"${MYSQL_ROOT_PASSWORD}" < "/import_danych/nawigacja_gwiezdna.sql" || { echo "Schema import failed!"; exit 1; }
 
 echo "==================================="
 echo "2. Importing CSV data..."
 echo "==================================="
-mysql --local-infile=1 -u root -p"${MYSQL_ROOT_PASSWORD}" nawigacja_gwiezdna < "/import_danych/import_docker.sql"
+mysql --default-character-set=utf8mb4 --local-infile=1 -u root -p"${MYSQL_ROOT_PASSWORD}" nawigacja_gwiezdna < "/import_danych/import_docker.sql" || { echo "Data import failed!"; exit 1; }
 
 echo "==================================="
 echo "Database initialization completed."
